ec29a5c24043277e77915428c46631a81a0bdbf7
naokinaokiboo <naokinaokiboo@gmail.com>
2023-08-15 02:32:39 +0900

    ユーザーの一覧画面、詳細画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show', as: 'user'
   resources :books
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

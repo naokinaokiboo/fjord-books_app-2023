d0d8c140aa6d105305c5c39aac77b31554514215
naokinaokiboo <naokinaokiboo@gmail.com>
2023-08-14 17:56:14 +0900

    letter_opener_webの設定を追加
    ・ルート設定
    ・メールの配信方法

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :books
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

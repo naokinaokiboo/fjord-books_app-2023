74028d2df2b8fb27fa2aac594c232c1e5f94bf3c
naokinaokiboo <naokinaokiboo@gmail.com>
2023-08-24 23:51:47 +0900

    本、日報にコメントを投稿する機能を追加
    ・投稿、削除時にそれぞれ失敗したとき（save, destroy）の対応は未実装

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+  def create
+    comment = @commentable.comments.build(comment_params.merge(user_id: current_user.id))
+    if comment.save
+      redirect_to @commentable, notice: 'コメントが正常に投稿されました。'
+    else
+      # TODO
+    end
+  end
+
+  def destroy
+    comment = @commentable.comments.find(params[:id])
+    if comment.destroy
+      redirect_to @commentable, notice: 'コメントが正常に削除されました。'
+    else
+      # TODO
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end

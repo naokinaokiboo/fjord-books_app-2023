702bbbc02cae30788137557cb56e283e17b9add4
naokinaokiboo <naokinaokiboo@gmail.com>
2023-08-30 00:56:46 +0900

    ReportMentionモデルにバリデーションを追加

--- app/models/report_mention.rb
+++ app/models/report_mention.rb
@@ -3,4 +3,7 @@
 class ReportMention < ApplicationRecord
   belongs_to :mentioning_report, class_name: :Report
   belongs_to :mentioned_report, class_name: :Report
+
+  validates :mentioning_report_id, presence: true, uniqueness: { scope: :mentioned_report_id }
+  validates :mentioned_report_id, presence: true
 end

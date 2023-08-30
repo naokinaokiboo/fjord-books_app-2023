d1bbc884c11a1b5f6ac73fa1cffd863c7fd8701e
naokinaokiboo <naokinaokiboo@gmail.com>
2023-08-30 11:05:31 +0900

    ReportMentionsテーブルにNOT NULL制約を追加

--- db/migrate/20230828083140_create_report_mentions.rb
+++ db/migrate/20230828083140_create_report_mentions.rb
@@ -1,8 +1,8 @@
 class CreateReportMentions < ActiveRecord::Migration[7.0]
   def change
     create_table :report_mentions do |t|
-      t.references :mentioning_report, foreign_key: { to_table: :reports }
-      t.references :mentioned_report, foreign_key: { to_table: :reports }
+      t.references :mentioning_report, foreign_key: { to_table: :reports }, null: false
+      t.references :mentioned_report, foreign_key: { to_table: :reports }, null: false
 
       t.timestamps
       t.index [:mentioning_report_id, :mentioned_report_id], unique: true, name: :index_on_mentioning_report_id_and_mentioned_report_id

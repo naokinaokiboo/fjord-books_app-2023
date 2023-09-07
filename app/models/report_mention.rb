94c493a248eb898f3b63e2e6e05bbf8cff9c160c
naokinaokiboo <naokinaokiboo@gmail.com>
2023-09-07 18:10:01 +0900

    冗長なバリデーションの記載を削除

--- app/models/report_mention.rb
+++ app/models/report_mention.rb
@@ -4,6 +4,5 @@ class ReportMention < ApplicationRecord
   belongs_to :mentioning_report, class_name: :Report
   belongs_to :mentioned_report, class_name: :Report
 
-  validates :mentioning_report_id, presence: true, uniqueness: { scope: :mentioned_report_id }
-  validates :mentioned_report_id, presence: true
+  validates :mentioning_report_id, uniqueness: { scope: :mentioned_report_id }
 end
